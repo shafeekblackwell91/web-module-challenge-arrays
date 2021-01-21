7e5021605d3e349bda8679369fffaf6680e539ca
Shafeek <shafeekblackwell91@gmail.com>
2021-01-20 22:41:02 -0600

    solved filter ByWord function

--- index.js
+++ index.js
@@ -160,11 +160,16 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(arr, string){
+    const filteredArray = [];
+    for(let i = 0; i <arr, lenth; i++){
+        if(arr[i].includes(string)){
+            filteredArray.push(arr[i]);
+        }
+    }
+    return filteredArray;
 }
-
-
+console.log(filterByWord(originalFlavors, 'Chocolate'));
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
 
 /* STRETCH 1: Write a function that returns the average number of words in an array. You should be able to use this function for any array, but can test with originalFlavors.
